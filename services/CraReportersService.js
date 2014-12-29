c2a9fc46bb632c846a70151fc90f487b11dbb748
Donald Oellerich <doelleri@gmail.com>
2014-12-29 13:09:03 -0500

    need to add data before query will work

--- services/CraReportersService.js
+++ services/CraReportersService.js
@@ -4,9 +4,7 @@ var CraReporters = require('../models/craReporters');
 
 module.exports = {
     isCraReporter: function(activityYear, respondentId, callback) {
-        console.log('isCraReporter');
         CraReporters.count({ 'activity_year': activityYear, 'data.respondent_id': respondentId }, function(err, count) {
-            console.log('callback');
             if (err) {
                 return callback(err, null);
             }
--- test/services/CraReportersServiceSpec.js
+++ test/services/CraReportersServiceSpec.js
@@ -7,16 +7,14 @@ describe('CraReportersService', function() {
     describe('isCraReporter', function() {
         it('should return a result of true if the respondent ID is present for the activity year', function(done) {
             CraReportersService.isCraReporter('2013', '0000000001', function(err, result) {
-                console.log(result);
-                expect(result).to.be.true();
+                expect(result.result).to.be.true();
                 done();
             });
         });
 
         it('should return a result of false if the respondent ID is not present for the activity year', function(done) {
             CraReportersService.isCraReporter('2013', '0000000002', function(err, result) {
-                console.log(result);
-                expect(result).to.be.false();
+                expect(result.result).to.be.false();
                 done();
             });
         });
