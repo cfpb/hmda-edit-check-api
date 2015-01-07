beba6a470953b6d1b6b484a748e609299691899f
Donald Oellerich <doelleri@gmail.com>
2015-01-07 12:00:31 -0500

    Add test for not matching the route

--- test/controllers/isCraReporterSpec.js
+++ test/controllers/isCraReporterSpec.js
@@ -4,14 +4,23 @@
 
 describe('/isCraReporter', function() {
 
-    it('should return true if a reporter is valid', function(done) {
+    it('should return a result if the request is valid', function(done) {
         request(mock)
             .get('/isCraReporter/2013/0000000001')
             .expect(200)
             .expect('Content-Type', /json/)
 
-                .expect(/"result":true/)
+                .expect(/"result":/)
 
+            .end(function (err, res) {
+                done(err);
+            });
+    });
+
+    it('should return 404 if a request doesn\'t match the route', function(done) {
+        request(mock)
+            .get('/isCraReporter/2013/')
+            .expect(404)
             .end(function (err, res) {
                 done(err);
             });
