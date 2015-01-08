bdb281be5c11c5c2a3f11db9a9367d9a3544e8d7
Donald Oellerich <doelleri@gmail.com>
2015-01-08 12:46:50 -0500

    flatten schemas

--- models/craReporters.js
+++ models/craReporters.js
@@ -4,11 +4,9 @@ var mongoose = require('mongoose');
 
 var craReportersSchema = mongoose.Schema({
     'activity_year': String,
-    'data': [{
-        'respondent_id': String,
-        'agency_code': String,
-        'respondent_name': String
-    }]
+    'respondent_id': String,
+    'agency_code': String,
+    'respondent_name': String
 });
 
 module.exports = mongoose.model('CraReporters', craReportersSchema, 'cra_reporters');
--- models/panel.js
+++ models/panel.js
@@ -4,15 +4,13 @@ var mongoose = require('mongoose');
 
 var panelSchema = mongoose.Schema({
     'activity_year': String,
-    'data': [{
-        'respondent_id': String,
-        'agency_code': String,
-        'respondent_name': String,
-        'parent_name': String,
-        'parent_city': String,
-        'parent_state': String,
-        'other_lender_code': String
-    }]
+    'respondent_id': String,
+    'agency_code': String,
+    'respondent_name': String,
+    'parent_name': String,
+    'parent_city': String,
+    'parent_state': String,
+    'other_lender_code': String
 });
 
 module.exports = mongoose.model('Panel', panelSchema, 'panel');
