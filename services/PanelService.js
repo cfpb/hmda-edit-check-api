7e0fd8597fc949a805b52e6ba2c001297766d010
Donald Oellerich <doelleri@gmail.com>
2015-01-09 11:22:01 -0500

    update to match flattened schema

--- services/PanelService.js
+++ services/PanelService.js
@@ -4,7 +4,7 @@ var Panel = require('../models/panel');
 
 module.exports = {
     isChildFI: function(activityYear, respondentId, callback) {
-        Panel.count({ 'activity_year': activityYear, 'data': { '$elemMatch': { 'respondent_id': respondentId, 'parent_name': { '$ne': '' }, 'other_lender_code': '0' } } }, function(err, count) {
+        Panel.count({ 'activity_year': activityYear, 'respondent_id': respondentId, 'parent_name': { '$ne': '' }, 'other_lender_code': '0' }, function(err, count) {
             if (err) {
                 return callback(err, null);
             }
