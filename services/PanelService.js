4c006269711c2d1bd76639b497b9543757ebfc66
Donald Oellerich <doelleri@gmail.com>
2015-01-20 16:42:34 -0500

    refactor panel queries to a common method and implement isValidControlNumber

--- services/PanelService.js
+++ services/PanelService.js
@@ -2,30 +2,32 @@
 
 var Panel = require('../models/panel');
 
+var count = function(query, callback) {
+    Panel.count(query, function(err, count) {
+        if (err) {
+            return callback(err, null);
+        }
+        var result = { result: false };
+        if (count) {
+            result.result = true;
+        }
+        return callback(null, result);
+    });
+};
+
 module.exports = {
     isChildFI: function(activityYear, respondentId, callback) {
-        Panel.count({ 'activity_year': activityYear, 'respondent_id': respondentId, 'parent_name': { '$ne': '' }, 'other_lender_code': '0' }, function(err, count) {
-            if (err) {
-                return callback(err, null);
-            }
-            var result = { result: false };
-            if (count) {
-                result.result = true;
-            }
-            return callback(null, result);
-        });
+        var query = { 'activity_year': activityYear, 'respondent_id': respondentId, 'parent_name': { '$ne': '' }, 'other_lender_code': '0' };
+        count(query, callback);
     },
 
     isRespondentMBS: function(activityYear, respondentId, callback) {
-        Panel.count({ 'activity_year': activityYear, 'respondent_id': respondentId, 'parent_name': { '$ne': '' }, 'other_lender_code': { '$in': ['1', '2', '3'] } }, function(err, count) {
-            if (err) {
-                return callback(err, null);
-            }
-            var result = { result: false };
-            if (count) {
-                result.result = true;
-            }
-            return callback(null, result);
-        });
+        var query = { 'activity_year': activityYear, 'respondent_id': respondentId, 'parent_name': { '$ne': '' }, 'other_lender_code': { '$in': ['1', '2', '3'] } };
+        count(query, callback);
+    },
+
+    isValidControlNumber: function(activityYear, agencyCode, respondentId, callback) {
+        var query = { 'activity_year': activityYear, 'agency_code': agencyCode, 'respondent_id': respondentId };
+        count(query, callback);
     }
 };
