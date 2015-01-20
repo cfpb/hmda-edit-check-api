c17762c9a83446ac5dabb4e89028faad2d726ae4
Donald Oellerich <doelleri@gmail.com>
2015-01-20 15:49:30 -0500

    fix path for test

--- test/controllers/isChildFISpec.js
+++ test/controllers/isChildFISpec.js
@@ -6,7 +6,7 @@ describe('/isCraReporter', function() {
 
     it('should return a result if the request is valid', function(done) {
         request(mock)
-            .get('/isCraReporter/2013/0000000001')
+            .get('/isChildFI/2013/0000000001')
             .expect(200)
             .expect('Content-Type', /json/)
 
