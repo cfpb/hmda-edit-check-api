28a34611569b0bc1d15d02b68acdfc7343c7d6aa
porterbot <david.schauder@collab.cfpb.gov>
2015-01-30 11:15:00 -0500

    fixed doc string and deprecated error return

--- controllers/isValidTimestamp.js
+++ controllers/isValidTimestamp.js
@@ -5,13 +5,13 @@ var TransmittalService = require('../services/TransmittalService');
 module.exports = function(router) {
 
     /**
-     * @param {String} activityYear, {String} msa
+     * @param {String} activityYear, {String} respondentID, {String} timestamp
      * @return {json}
      */
      router.get('/:activityYear/:respondentID/:timestamp', function(req, res) {
          TransmittalService.isValidTimestamp(req.params.activityYear, req.params.respondentID, req.params.timestamp, function(err, result) {
              if (err) {
-                 res.json(500, err);
+                 res.status(500).json(err);
              } else {
                  res.json(result);
              }
