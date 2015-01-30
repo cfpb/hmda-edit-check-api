9be479e36c63d614ed9cf7f85f1dd2afc678a4c1
porterbot <david.schauder@collab.cfpb.gov>
2015-01-30 17:48:59 -0500

    one more test

--- test/controllers/isValidTimestamp.js
+++ test/controllers/isValidTimestamp.js
@@ -30,6 +30,18 @@ describe('/isValidTimestamp', function() {
             });
     });
 
+    it('should return false for missing year', function(done) {
+        request(mock)
+            .get('/isValidTimestamp/2010/0000000001/201401100000')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(/"result":false/)
+
+            .end(function (err, res) {
+                done(err);
+            });
+    });
+
     it('should return a 500 if there is a problem', function(done) {
         mockgoose.setMockReadyState(mongoose.connection, 0);
 
