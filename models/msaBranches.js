322f98f5be7843fc6f4ecdb789ca129d2a76f0b9
Stephen Kent <stevejameskent1@gmail.com>
2015-02-18 16:55:37 -0500

    Add agency_code to schemas and update indexes to include it

--- models/msaBranches.js
+++ models/msaBranches.js
@@ -5,8 +5,9 @@ var mongoose = require('mongoose');
 var msaBranchesSchema = mongoose.Schema({
     'activity_year': String,
     'respondent_id': String,
+    'agency_code': String,
     'respondent_name': String,
     'msa': [String]
 });
-msaBranchesSchema.index({'activity_year': 1, 'respondent_id': 1});
+msaBranchesSchema.index({'activity_year': 1, 'respondent_id': 1, 'agency_code': 1});
 module.exports = mongoose.model('MsaBranches', msaBranchesSchema, 'msa_branches');
