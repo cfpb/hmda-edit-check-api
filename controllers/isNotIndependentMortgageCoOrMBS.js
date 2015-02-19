6e4d5613964a46b09a1c190a10953ae3ec21a413
Donald Oellerich <doelleri@gmail.com>
2015-02-19 15:46:48 -0500

    isNotIndependentMortgageCoOrMBS, first part of the API for Q595

---
+++ controllers/isNotIndependentMortgageCoOrMBS.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var PanelService = require('../services/PanelService');
+
+module.exports = function(router) {
+
+    /**
+     * @param {String} activityYear, {String} agencyCode, {String} respondentId
+     * @return {json}
+     */
+    router.get('/:activityYear/:agencyCode/:respondentId', function(req, res) {
+        PanelService.isNotIndependentMortgageCoOrMBS(req.params.activityYear, req.params.agencyCode, req.params.respondentId, function(err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.json(result);
+            }
+        });
+    });
+};
---
+++ test/controllers/isNotIndependentMortgageCoOrMBSSpec.js
@@ -0,0 +1,32 @@
+/*global describe:false, it:false, beforeEach:false, afterEach:false, request:false, mock:false*/
+'use strict';
+
+var mongoose = require('mongoose'),
+    mockgoose = require('mockgoose');
+
+describe('/isNotIndependentMortgageCoOrMBS', function() {
+    it('should return a result if the request is valid', function(done) {
+        request(mock)
+            .get('/isNotIndependentMortgageCoOrMBS/2013/1/0000000001')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(/"result":/)
+            .end(function (err, res) {
+                done(err);
+            });
+    });
+
+    it('should return a 500 if there is a problem', function(done) {
+        mockgoose.setMockReadyState(mongoose.connection, 0);
+
+        request(mock)
+            .get('/isNotIndependentMortgageCoOrMBS/2013/1/0000000001')
+            .expect(500)
+            .expect('Content-Type', /json/)
+            .expect(/"code":/)
+            .end(function (err, res) {
+                mockgoose.setMockReadyState(mongoose.connection, 1);
+                done(err);
+            });
+    });
+});
