4081e12711bd424bf1b7f9056cf8106d4e316197
Stephen Kent <stevejameskent1@gmail.com>
2015-02-19 10:24:35 -0500

    Update tests for PanelService and TransmittalService

--- test/services/TransmittalServiceSpec.js
+++ test/services/TransmittalServiceSpec.js
@@ -6,21 +6,21 @@ var TransmittalService = require('../../services/TransmittalService');
 describe('TransmittalService', function() {
     describe('isValidTimestamp', function() {
         it('should return true for a valid timestamp more than one day after January 1, 2014', function(done) {
-            TransmittalService.isValidTimestamp('2014', '0000000001', '201401100000', function(err, result) {
+            TransmittalService.isValidTimestamp('2014', '1', '0000000001', '201401100000', function(err, result) {
                 expect(result.result).to.be.true();
                 done();
             });
         });
 
         it('should return false for an timestamp less than one day after January 1, 2014', function(done) {
-            TransmittalService.isValidTimestamp('2014', '0000000001', '201401011800', function(err, result) {
+            TransmittalService.isValidTimestamp('2014', '1', '0000000001', '201401011800', function(err, result) {
                 expect(result.result).to.be.false();
                 done();
             });
         });
 
         it('should return false for an timestamp before January 1, 2014', function(done) {
-            TransmittalService.isValidTimestamp('2014', '0000000001', '201311011800', function(err, result) {
+            TransmittalService.isValidTimestamp('2014', '1', '0000000001', '201311011800', function(err, result) {
                 expect(result.result).to.be.false();
                 done();
             });
@@ -29,14 +29,14 @@ describe('TransmittalService', function() {
 
     describe('isTaxIDTheSameAsLastYear', function() {
         it('should return true when taxID is the same as last year', function(done) {
-            TransmittalService.isTaxIDTheSameAsLastYear('2014', '0000000001', '23-0916895', function(err, result) {
+            TransmittalService.isTaxIDTheSameAsLastYear('2014', '1', '0000000001', '23-0916895', function(err, result) {
                 expect(result.result).to.be.true();
                 done();
             });
         });
 
         it('should return false when taxID is not the same as last year', function(done) {
-           TransmittalService.isTaxIDTheSameAsLastYear('2014', '0000000001', '22-0916895', function(err, result) {
+           TransmittalService.isTaxIDTheSameAsLastYear('2014', '1', '0000000001', '22-0916895', function(err, result) {
                 expect(result.result).to.be.false();
                 done();
             }); 
