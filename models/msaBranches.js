e3fd9c4d2405eedac029a0584b050d8230f6d0a9
Donald Oellerich <doelleri@gmail.com>
2015-02-19 16:51:32 -0500

    update index for what's actually queried

--- models/msaBranches.js
+++ models/msaBranches.js
@@ -9,5 +9,5 @@ var msaBranchesSchema = mongoose.Schema({
     'msa': [String],
     'agency_code': String
 });
-msaBranchesSchema.index({'activity_year': 1, 'respondent_id': 1});
+msaBranchesSchema.index({'activity_year': 1, 'respondent_id': 1, 'agency_code': 1, 'msa': 1});
 module.exports = mongoose.model('MsaBranches', msaBranchesSchema, 'msa_branches');
