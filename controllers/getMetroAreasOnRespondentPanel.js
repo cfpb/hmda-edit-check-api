80a94eb4902a0646160ec886bfdd096a0297cd51
David Schauder <porterbot@github.com>
2015-03-23 13:11:17 -0400

    added new controller for getting MSA branches

---
+++ controllers/getMetroAreasOnRespondentPanel.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var MsaBranchesService = require('../services/MsaBranchesService');
+
+module.exports = function(router) {
+
+    /**
+     * @api {get} /getMetroAreasOnRespondentPanel/:activityYear/:agencyCode/:respondentId getMetroAreasOnRespondentPanel
+     * @apiDescription returns a list of the metro areas where the respondent has a home or branch office
+     * @apiGroup MSA Branches
+     * @apiParam {String} activityYear The year for which the HMDA data are being collected
+     * @apiParam {String} agencyCode Code to identify the supervisory/regulatory agency of the HMDA reporting institution
+     * @apiParam {String} respondentId Ten-digit number used to identify a HMDA reporting institution
+     */
+    router.get('/:activityYear/:agencyCode/:respondentId', function(req, res) {
+        MsaBranchesService.getMetroAreasOnRespondentPanel(req.params.activityYear, req.params.agencyCode, 
+            req.params.respondentId, function(err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.json(result);
+            }
+        });
+    });
+};
---
+++ test/controllers/getMetroAreasOnRespondentPanelSpec.js
@@ -0,0 +1,32 @@
+/*global describe:false, it:false, beforeEach:false, afterEach:false, request:false, mock:false*/
+'use strict';
+
+var mongoose = require('mongoose'),
+    mockgoose = require('mockgoose');
+
+describe('/getMetroAreasOnRespondentPanel', function() {
+    it('should return a result if the request is valid', function(done) {
+        request(mock)
+            .get('/getMetroAreasOnRespondentPanel/2013/1/0000000001')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(/"msa":/)
+            .end(function (err, res) {
+                done(err);
+            });
+    });
+
+    it('should return a 500 if there is a problem', function(done) {
+        mockgoose.setMockReadyState(mongoose.connection, 0);
+
+        request(mock)
+            .get('/getMetroAreasOnRespondentPanel/2013/1/0000000001')
+            .expect(500)
+            .expect('Content-Type', /json/)
+            .expect(/"code":/)
+            .end(function (err, res) {
+                mockgoose.setMockReadyState(mongoose.connection, 1);
+                done(err);
+            });
+    });
+});
