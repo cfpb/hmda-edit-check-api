861817491380b608cd89dcd952962e9179f2f9a4
David Schauder <porterbot@github.com>
2015-04-15 10:13:44 -0400

    fixed issue with null when previous year totals not found

--- test/services/LARServiceSpec.js
+++ test/services/LARServiceSpec.js
@@ -131,6 +131,20 @@ describe('LARService', function() {
             });
         });
 
+        it('should return true when previous year does not exist and this year has < 500 loans', function(done) {
+            LARService.isValidNumLoans('2013', '9', '1201547730', 359, function(err, result) {
+                expect(result.result).to.be.true();
+                done();
+            });
+        });
+
+        it('should return false when previous year does not exist and this year has > 500 loans', function(done) {
+            LARService.isValidNumLoans('2013', '9', '1201547730', 750, function(err, result) {
+                expect(result.result).to.be.false();
+                done();
+            });
+        });
+
         it('should return true for a valid percent when only one year has > 500 loans', function(done) {
            LARService.isValidNumLoans('2013', '9', '1201547730', 501, function(err, result) {
                 expect(result.result).to.be.true();
